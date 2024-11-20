32de8065a70541eac3fcbec8c055f70be770ff79
ApiCat <tvglavatskaya@gmail.com>
2024-11-20 13:01:11 +0300

    Добавлена метка с названием базовой валюты

--- Obmen.py
+++ Obmen.py
@@ -3,29 +3,39 @@ from tkinter import messagebox as mb
 import requests
 from tkinter import ttk
 
-def update_c_label(event):
-    code=combobox.get()
+
+def update_b_label(event):
+    code=b_combobox.get()
     name=cur[code]
-    c_label.config(text=name)
+    b_label.config(text=name)
+
+
+def update_t_label(event):
+    code=t_combobox.get()
+    name=cur[code]
+    t_label.config(text=name)
 
 cur={"EUR": "Евро","JPY": "Японская йена","GBP": "Британский фунт стерлингов",
 "AUD": "Австралийский доллар","CAD": "Канадский доллар","CHF": "Швейцарский франк",
-"CNY": "Китайский юань","RUB": "Российский рубль","KZT": "Казахстанский тенге","UZS": "Узбекский сум"}
+"CNY": "Китайский юань","RUB": "Российский рубль","KZT": "Казахстанский тенге",
+"UZS": "Узбекский сум", "USD": "Амеирканский доллар"}
 
 def exchange():
-    code = combobox.get().upper()
-    if code:
+    t_code = t_combobox.get().upper()
+    b_code = b_combobox.get().upper()
+    if t_code and b_code:
         try:
-            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response = requests.get(f"https://open.er-api.com/v6/latest/{b_code}")
             response.raise_for_status()
             data = response.json()
-            if code in data["rates"]:
-                exchange_rate = data["rates"][code]
-                c_name=cur[code]
+            if t_code in data["rates"]:
+                exchange_rate = data["rates"][t_code]
+                t_name=cur[t_code]
+                b_name=cur[b_code]
                 mb.showinfo("Курс обмена",
-                            f"Курс к доллару: {exchange_rate:.2f} {c_name} за 1 доллар")
+                            f"Курс: {exchange_rate:.2f} {t_name} за 1 {b_name}")
             else:
-                mb.showerror("Ошибка", f"Валюта {code} не найдена")
+                mb.showerror("Ошибка", f"Валюта {t_code} не найдена")
         except Exception as e:
             mb.showerror("Ошибка", f"Произошла ошибка {e}")
     else:
@@ -34,19 +44,23 @@ def exchange():
 
 window = Tk()
 window.title("Курс обмена валют")
-window.geometry("360x180")
-
-Label(text="Выберите код валюты: ").pack()
+window.geometry("350x350")
 
 
-combobox=ttk.Combobox(values=list(cur.keys()))
-combobox.pack(padx=10,pady=10)
-combobox.bind("<<ComboboxSelected>>",update_c_label)
+Label(text="Базовая валюта").pack(padx=10,pady=10)
+b_combobox=ttk.Combobox(values=list(cur.keys()))
+b_combobox.pack(padx=10,pady=10)
+b_combobox.bind("<<ComboboxSelected>>",update_b_label)
+b_label=ttk.Label()
+b_label.pack(padx=10,pady=10)
 
 
-
-c_label=ttk.Label()
-c_label.pack(padx=10,pady=10)
+Label(text="Целевая валюта").pack(padx=10,pady=10)
+t_combobox=ttk.Combobox(values=list(cur.keys()))
+t_combobox.pack(padx=10,pady=10)
+t_combobox.bind("<<ComboboxSelected>>",update_t_label)
+t_label=ttk.Label()
+t_label.pack(padx=10,pady=10)
 
 Button(text="Получить курс обмена", command=exchange).pack()
 
